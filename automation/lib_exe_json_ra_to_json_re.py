0c9ed306eb04dd867b7435dabd4de999d574038b
lucathe <lucastheo.g.a@gmail.com>
2020-05-23 18:01:41 -0300

    Adicionando reexecutar tudo

--- automation/lib_exe_json_ra_to_json_re.py
+++ automation/lib_exe_json_ra_to_json_re.py
@@ -1,15 +1,26 @@
 import os
 PATH_DOWN_HTML = '../json-ra-to-json-re/'
 PYTHON = 'python '
-def execute():
+def execute(reexecute = False):
+    if reexecute == False:
+        gen_jre_most_cited_new()
+    else:
+        gen_jre_most_cited()
 
-    gen_jre_most_cited()
+def gen_jre_most_cited_new():
+    f = 'gen_jre_most_cited.py'
+
+    s = 'cd ' + PATH_DOWN_HTML + ';'
+    s += PYTHON + f + ' &\n'
+    s += PYTHON + f + ' --data --new &\n'
+    s += PYTHON + f + ' --url --new'
+    os.system( s )
 
 def gen_jre_most_cited():
     f = 'gen_jre_most_cited.py'
 
     s = 'cd ' + PATH_DOWN_HTML + ';'
-    s += PYTHON + f + ' &&\n'
-    s += PYTHON + f + ' --data --new &&\n'
-    s += PYTHON + f + ' --url --new'
+    s += PYTHON + f + ' &\n'
+    s += PYTHON + f + ' --data &\n'
+    s += PYTHON + f + ' --url '
     os.system( s )
--- automation/main.py
+++ automation/main.py
@@ -5,8 +5,9 @@ import lib_exe_json_ra_to_json_re
 import lib_exe_json_re_to_plot
 
 if __name__ == "__main__":
+    print("[INIT ]-------------------------------------------------------------------------")
     lib_exe_down_html.execute()
     lib_exe_html_to_json_ra.execute()
-    lib_exe_json_ra_to_json_re.execute()
-    lib_exe_json_re_to_plot.execute()
-
+    lib_exe_json_ra_to_json_re.execute(reexecute=False)
+    lib_exe_json_re_to_plot.execute(reexecute=False)
+    print("[EXIT ]-------------------------------------------------------------------------")
