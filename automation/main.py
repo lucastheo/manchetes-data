795069956615043dff58d948b9f2a07bb9ffb249
lucathe <lucastheo.g.a@gmail.com>
2020-08-06 22:58:55 -0300

    Deixando no padrão

--- automation/main.py
+++ automation/main.py
@@ -12,10 +12,10 @@ def get_time_now():
 if __name__ == "__main__":
     print("[INIT ]-------------------------------------------------------------------------")
     print("[DATA ]" , get_time_now() )
-    #lib_exe_down_html.execute()
+    lib_exe_down_html.execute()
     lib_exe_html_to_json_ra.execute()
-    lib_exe_json_ra_to_data_base.execute(True)
-    lib_exe_data_base_to_json_re.execute(True)
+    lib_exe_json_ra_to_data_base.execute(False)
+    lib_exe_data_base_to_json_re.execute(False)
     #lib_exe_json_ra_to_json_re.execute(reexecute=True)
     #lib_exe_json_re_to_plot.execute(reexecute=True)
     print("[EXIT ]-------------------------------------------------------------------------")
